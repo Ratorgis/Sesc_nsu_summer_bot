57af98b60f3e1c1f553e1786002849c3a877df6d
Ratorgis <ryabovm07@mail.ru>
2025-08-06 19:24:32 +0700

    Version for hostion on server

--- create_bot.py
+++ create_bot.py
@@ -12,8 +12,5 @@ admins = [int(admin) for admin in os.getenv('ADMINS').split(",")]
 channel_id = os.getenv('GROUP_ID')
 news_channel_id = os.getenv('NEWS_CHANELL_ID')
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
 bot = Bot(token = os.getenv('TOKEN'), default = DefaultBotProperties(parse_mode = ParseMode.HTML))
 dp = Dispatcher(storage = MemoryStorage())
